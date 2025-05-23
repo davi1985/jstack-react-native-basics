0fb3d49bd642df34f53046ff6731e9eaa857a57c
Davi Silva <davisilvaphoto@gmail.com>
2025-05-23 13:39:33 -0300

    start

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
   return (
-    <View style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
-      <Text>Hello JStack</Text>
+    <View style={styles.container}>
+      <Text>
+        Hello JStack
+        <Text style={styles.span}>React Native</Text>
+      </Text>
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  span: {
+    fontWeight: 'bold',
+  },
+});
