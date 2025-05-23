8431aaa98f0a245758b7c7988eff8098b3bda7f4
Davi Silva <davisilvaphoto@gmail.com>
2025-05-23 14:34:21 -0300

    scrollview - properties android only and ios only

--- App.tsx
+++ App.tsx
@@ -2,15 +2,32 @@ import {ScrollView, Text} from 'react-native';
 
 const App = () => {
   return (
-    <ScrollView alwaysBounceVertical={false}>
-      <Text style={{marginTop: 200, fontSize: 32, width: 1000}}>Hello</Text>
-
-      <ScrollView horizontal alwaysBounceHorizontal={false}>
+    <ScrollView
+      persistentScrollbar // * Android only
+      indicatorStyle={'black'} // * iOS only
+      showsVerticalScrollIndicator={false}>
+      <ScrollView
+        horizontal
+        style={{marginTop: 100}}
+        showsHorizontalScrollIndicator={false}>
         <Text style={{fontSize: 32}}>
           JStack - the best React and JavaScript course JStack - the best React
           and JavaScript course
         </Text>
       </ScrollView>
+
+      <Text style={{marginTop: 50, fontSize: 32}}>
+        JStack - the best React and JavaScript course JStack - the best React
+        and JavaScript course JStack - the best React and JavaScript course
+        JStack - the best React and JavaScript course JStack - the best React
+        and JavaScript course JStack - the best React and JavaScript course
+        JStack - the best React and JavaScript course JStack - the best React
+        and JavaScript course JStack - the best React and JavaScript course
+        JStack - the best React and JavaScript course JStack - the best React
+        and JavaScript course JStack - the best React and JavaScript course
+        JStack - the best React and JavaScript course JStack - the best React
+        and JavaScript course JStack - the best React and JavaScript course
+      </Text>
     </ScrollView>
   );
 };
